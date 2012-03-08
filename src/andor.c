3d09900303ab36c7cbe55ba8d6f2453d0a12edac
Alastair Basden <a.g.basden@durham.ac.uk>
2012-03-08 14:32:43 +0000

    debug

--- src/andor.c
+++ src/andor.c
@@ -631,7 +631,7 @@ int camOpen(char *name,int n,int *args,paramBuf *pbuf,circBuf *rtcErrorBuf,char 
     *camHandle=NULL;
     return 1;
   }
-  if((camstr->started=malloc(sizeof(int)*ncam))==NULL){
+  if((camstr->started=calloc(sizeof(int),cam))==NULL){
     printf("Error allocing camstr->started\n");
     free(camstr->paramNames);
     free(camstr->cammap);
