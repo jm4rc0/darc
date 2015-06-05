4507451f4b6592b5010f9fad7a317cc136c81875
Ali Basden <a.g.basden@durham.ac.uk>
2015-06-05 20:34:20 +0100

    debug

--- src/mirrorPdAO32AlpaoThreaded.c
+++ src/mirrorPdAO32AlpaoThreaded.c
@@ -393,6 +393,8 @@ void* workerAlpao(void *mirstrv){
     if(mirstr->open){
       mirstr->err=0;
       offset=0;
+      gettimeofday(&t1,NULL);
+      mirstr->mirrorframeno[1]=t1.tv_sec*1000000+t1.tv_usec;//gives some indicat
       if(mirstr->actMapping==NULL){
 	//and now for the alpao...
 	for(i=0;i<mirstr->nalpao;i++){
@@ -417,8 +419,8 @@ void* workerAlpao(void *mirstrv){
 	  offset+=mirstr->nactBoard[i+mirstr->nboards];
 	}
       }
-      gettimeofday(&t1,NULL);
-      mirstr->mirrorframeno[1]=t1.tv_sec*1000000+t1.tv_usec;//gives some indication as to whether we're sending to the dm at the AO frame rate (which won't be the case if asdkSend takes too long to complete).
+      //gettimeofday(&t1,NULL);
+      //mirstr->mirrorframeno[1]=t1.tv_sec*1000000+t1.tv_usec;//gives some indication as to whether we're sending to the dm at the AO frame rate (which won't be the case if asdkSend takes too long to complete).
     }
   }
   pthread_mutex_unlock(&mirstr->m2);
@@ -476,6 +478,8 @@ void* worker(void *mirstrv){
       mirstr->err=0;
       dmno=0;
       offset=0;
+      gettimeofday(&t1,NULL);
+      mirstr->mirrorframeno[0]=t1.tv_sec*1000000+tv_usec;//++;//gives some indic
 
       if(mirstr->actMapping==NULL){
 	for(i=0; i<mirstr->nactsBoard; i++){
@@ -485,18 +489,6 @@ void* worker(void *mirstrv){
 	  }
 	  mirstr->err|=_PdAO32Write(mirstr->handle[dmno],i-offset,mirstr->arr[i]);
 	}
-	//and now for the alpao...
-	offset=0;
-	for(i=0;i<mirstr->nalpao;i++){
-	  if(asdkSend(mirstr->alpaohandle[i],&mirstr->darr[offset])!=0){
-	    UInt errorNo;
-	    char errMsg[80];
-	    asdkGetLastError(&errorNo,errMsg,80);
-	    printf("%s\n",errMsg);
-	    printf("Error: asdkSend function\n");
-	  }
-	  offset+=mirstr->nactBoard[i+mirstr->nboards];
-	}
       }else{
 	for(i=0; i<mirstr->nactsBoard; i++){
 	  if(i-offset==mirstr->nactBoard[dmno]){
@@ -505,21 +497,9 @@ void* worker(void *mirstrv){
 	  }
 	  mirstr->err|=_PdAO32Write(mirstr->handle[dmno],mirstr->actMapping[i],mirstr->arr[i]);
 	}
-	//and now for the alpao (no actmapping here)
-	offset=0;
-	for(i=0;i<mirstr->nalpao;i++){
-	  if(asdkSend(mirstr->alpaohandle[i],&mirstr->darr[offset])!=0){
-	    UInt errorNo;
-	    char errMsg[80];
-	    asdkGetLastError(&errorNo,errMsg,80);
-	    printf("%s\n",errMsg);
-	    printf("Error: asdkSend function\n");
-	  }
-	  offset+=mirstr->nactBoard[i+mirstr->nboards];
-	}
       }
-      gettimeofday(&t1,NULL);
-      mirstr->mirrorframeno[0]=t1.tv_sec*1000000+t1.tv_usec;//++;//gives some indication as to whether we're sending to the dm at the AO frame rate (which won't be the case if asdkSend takes too long to complete).
+      //gettimeofday(&t1,NULL);
+      //mirstr->mirrorframeno[0]=t1.tv_sec*1000000+tv_usec;//++;//gives some indication as to whether we're sending to the dm at the AO frame rate (which won't be the case if asdkSend takes too long to complete).
     }
   }
   pthread_mutex_unlock(&mirstr->m);
