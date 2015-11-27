7c05250749da75be7c7027ae06ee3d4426abe866
Ali Basden <a.g.basden@durham.ac.uk>
2015-11-27 13:51:42 +0000

    Removed import darc.

--- lib/python/darccClient.py
+++ lib/python/darccClient.py
@@ -5,7 +5,6 @@ import subprocess
 import time
 import socket
 import numpy
-import darc
 import buffer
 import FITS
 #From darccontrolc:
@@ -204,7 +203,7 @@ class DarcCClient:
         #Start a receiver and get port number.
         port=self.startReceiver(stream)
         #Get the correct client IP - should be on same network as self.host
-        IPlist=[x[1] for x in darc.getNetworkInterfaces()]
+        IPlist=[x[1] for x in getNetworkInterfaces()]
         sendto=None
         for ip in IPlist:
             if ip.split(".")[:3]==self.host.split(".")[:3]:
@@ -248,4 +247,48 @@ class DarcCClient:
             outname=pyconfig[:-2]+"fits"
         buf=buffer.loadBufFromPyConfig(pyconfig,self.prefix,size,nhdr)
         buf.save(outname)
+
+
+
+def getNetworkInterfaces():
+    """
+    Used to get a list of the up interfaces and associated IP addresses
+    on this machine (linux only).
+
+    Returns:
+        List of interface tuples.  Each tuple consists of
+        (interface name, interface IP)
+    """
+
+    if os.environ.has_key("MYIPFORDARC"):
+        return [os.environ["MYIPFORDARC"]]
+    import fcntl
+    import array
+    import struct
+    import socket
+    import platform
+    SIOCGIFCONF = 0x8912
+    MAXBYTES = 8096
+
+    arch = platform.architecture()[0]
+
+    # I really don't know what to call these right now
+    var1 = -1
+    var2 = -1
+    if arch == '32bit':
+        var1 = 32
+        var2 = 32
+    elif arch == '64bit':
+        var1 = 16
+        var2 = 40
+    else:
+        print "Unable to obtain list of IP addresses.  You could try setting environment variable MYIPFORDARC = you IP address (probably you are on a mac?)"
+        raise OSError("Unknown architecture: %s" % arch)
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    names = array.array('B', '\0' * MAXBYTES)
+    outbytes = struct.unpack('iL', fcntl.ioctl(sock.fileno(),SIOCGIFCONF,struct.pack('iL', MAXBYTES, names.buffer_info()[0])))[0]
+    namestr = names.tostring()
+    return [(namestr[i:i+var1].split('\0', 1)[0], socket.inet_ntoa(namestr[i+20:i+24])) for i in xrange(0, outbytes, var2)]
+
 
